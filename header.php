d292a88146aadf20f59440ebd676182015768132
sinanisler <sinan@sinanisler.com>
2024-01-30 16:15:08 +0300

    always latest why not...

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title> <?php wp_title(  ); ?> </title>
 
-<script src="https://unpkg.com/htmx.org@1.9.10" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/htmx.org" crossorigin="anonymous"></script>
 <?php wp_head(); ?>
 </head>
 <body>
