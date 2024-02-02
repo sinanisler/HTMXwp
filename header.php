6e9ccf3ff7c39c1b833e659ed130f04e6cf2c73b
sinanisler <sinan@sinanisler.com>
2024-02-02 12:49:05 +0300

    head

--- header.php
+++ header.php
@@ -10,7 +10,10 @@
 		echo wp_title( '' );    }
 	?></title>
 
-<script src="https://unpkg.com/htmx.org" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/htmx.org@1.9.10" 
+	integrity="sha384-D1Kt99CQMDuVetoL1lrYwg5t+9QdHe7NLX/SoJYkXDFfX37iInKRy5xLSi8nO7UC" 
+	crossorigin="anonymous"></script>
+
 <?php wp_head(); ?>
 </head>
 <body>
